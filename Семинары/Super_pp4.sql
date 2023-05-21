d3d6acfe0e034b21586f310a740e53b9febbb5ed
Apanni <kymyx05@gmail.com>
2023-05-22 00:54:08 +0300

    обновление

---
+++ Семинары/Super_pp4.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Super_pp4 (
+	"Name" text,
+	"Gender" text,
+	"Eye color" text,
+	"Race" text,
+	"Hair color" text,
+	"Height" real,
+	"Publisher" text,
+	"Skin color" text,
+	"Alignment" text,
+	"Weight" real
+);
